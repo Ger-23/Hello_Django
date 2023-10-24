d7d5c504371fe015d3f12f7036f7b2a66c943a90
Ger-23 <>
2023-10-24 19:59:18 +0000

    Change from curly braces to parentheses in SECRET_KEY

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get{'SECRET_KEY', 'django-insecure-%7%zecy5@jbx=avv%h1%6(6@!#u#gc_l-_0h2jgku*g=3xq4qo'}
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-%7%zecy5@jbx=avv%h1%6(6@!#u#gc_l-_0h2jgku*g=3xq4qo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
