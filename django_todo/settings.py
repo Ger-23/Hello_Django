ad69304a94d74e15b27927894f9c5497a4e843f1
Ger-23 <>
2023-10-24 20:46:15 +0000

    Amend ALLOWED_HOSTS

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -35,7 +35,7 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['localhost']
 else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    ALLOWED_HOSTS = ['ckz87800-django-todo-app-84305268cf5b.herokuapp.com']
 
 
 # Application definition
